1942af8b45359b0e489a533b96b049dee6490844
shubham_sinha <shbkrsi@gmail.com>
2020-05-13 09:32:32 +0530

    class.py will give output of sum of numbers

--- class.py
+++ class.py
@@ -1,8 +1,7 @@
-class Fruit:
-	"""apple class describes flavor and color"""
-	def __init__(self, color, flavor):
-		self.color = color
-		self.flavor = flavor
-	def __str__(self):
-		return "This apple is {} and it's flavor is {}.".format(self.color, self.flavor)
-
+def function(number):
+	sums = 0
+	for i in range(number):
+		sums += i
+	return sums
+hgu = int(input())
+print(function(hgu))
