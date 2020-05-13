9d64b7a14a0e42ece8f6e8966faa870c9a8fcfd5
shubham-kumar-sinha <59770692+shubham-kumar-sinha@users.noreply.github.com>
2020-05-13 15:16:41 +0530

    update class.py

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 class Fruit:
-	"""apple class describes flavor and color"""
+	"""Fruit class describes flavor and color"""
 	def __init__(self, color, flavor):
 		self.color = color
 		self.flavor = flavor
